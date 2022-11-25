a2279f585437f63502688ca6180f94d451c308f8
Vinicius Parizoto <vinicius@clientbase.com.br>
2022-11-25 12:50:11 -0300

    Feat: Checkbox component change

--- src/components/Checkbox/Checkbox.interface.ts
+++ src/components/Checkbox/Checkbox.interface.ts
@@ -4,6 +4,7 @@ import React from 'react'
 interface ExtendCheckBox {
   isActive?: boolean
   children?: React.ReactNode
+  error?: boolean
 }
 
 export type CheckboxProps = BoxProps & ExtendCheckBox
--- src/components/Checkbox/Checkbox.styles.tsx
+++ src/components/Checkbox/Checkbox.styles.tsx
@@ -2,12 +2,23 @@ import { Box, styled } from '@mui/material'
 
 import { CheckboxProps } from '.'
 
-export const Checkbox = styled(Box)<CheckboxProps>(({ theme, isActive }) => ({
-  display: 'flex',
-  justifyContent: 'center',
-  border: `2px solid ${
-    isActive ? theme.palette.primary.main : theme.palette.grey[900]
-  }`,
-  color: isActive ? theme.palette.primary.main : theme.palette.grey[900],
-  padding: '10px 12px'
-}))
+export const Checkbox = styled(Box)<CheckboxProps>(
+  ({ theme, isActive, error }) => ({
+    display: 'flex',
+    cursor: 'pointer',
+    justifyContent: 'center',
+    border: `2px solid ${
+      error
+        ? theme.palette.error.main
+        : isActive
+        ? theme.palette.primary.main
+        : theme.palette.grey[900]
+    }`,
+    color: error
+      ? theme.palette.error.main
+      : isActive
+      ? theme.palette.primary.main
+      : theme.palette.grey[900],
+    padding: '10px 12px'
+  })
+)
